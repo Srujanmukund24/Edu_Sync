ca17b4553a438b329dc48952cb216b361621586f
Sumit <sumitsunke04@gmail.com>
2024-04-09 10:22:38 +0530

    removed add subject,practical from sumit branch

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,6 @@ app.post('/registerStudent',studentController.registerStudent);
 app.post('/loginStudent',studentController.loginStudent);
 app.post('/loginTeacher',teacherController.loginTeacher);
 app.post('/loginAdmin',adminController.loginAdmin);
-app.post('/addStudentSubjectInfo',auth.authorizeAdmin,adminController.addStudentSubjectInfo);
 app.delete('/removeTeacher/:regid', adminController.removeTeacher);
 app.delete('/removeStudent/:regid', adminController.removeStudent);
 
@@ -72,8 +71,6 @@ app.get('/myDivisions',auth.authorizeTeacher,teacherController.getMyDivisions);
 app.get('/myBatchs',auth.authorizeTeacher,teacherController.getMyBatches);
 
 app.post('/createAssignment',auth.authorizeTeacher,teacherController.createAssignment);
-app.post('/addStudentPracticalInfo',adminController.addStudentPracticalInfo);
-
 app.get('/getAssignments',auth.authorizeStudent,studentController.getAssignments);
 app.get('/getCompleted',auth.authorizeStudent,studentController.getCompletedAssignments);
 app.get('/getIncomplete',auth.authorizeStudent,studentController.getIncompleteAssignments);
